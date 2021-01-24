ba2123ad098e20c806edcf936d9bde6cc4fa0b37
Chris Baum <christopher.baum@valpo.edu>
2021-01-24 01:15:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,10 @@ class School
   end
 
   def add_student(name, grade)
-      @current_roster[:grade] << name
+      @current_roster = {
+        grade => {
+          name: [name]
+        }
+      }
   end
 end
